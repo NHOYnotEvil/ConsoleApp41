57e346f0265fe13fd521e9ff40dd232682d660e3
NHOY <NHOY@2210DR000919>
2023-05-30 01:18:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp41/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp41
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i < 97; i+= 7)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
